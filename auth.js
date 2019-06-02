ec4120e3385b75aefd593382aa9c8c72130660ea
Rahul <44020002+rahulmadaan@users.noreply.github.com>
2019-06-02 12:22:30 +0530

    Getting user details

--- auth.js
+++ auth.js
@@ -26,10 +26,21 @@ const processGithubRequest = function(req, res) {
     })
     .set("Accept", "application/json")
     .then(function(response) {
-      console.log(response.body);
+      getUserDetails(response.body.access_token, res);
     });
 };
-
+const getUserDetails = function(token, res) {
+  request
+    .get("https://api.github.com/user")
+    .set({
+      Authorization: `token ${token}`,
+      "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0"
+    })
+    .then(function(response) {
+      res.send(response.body);
+    });
+};
 app.use(logger);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
