aae180b4d0b585f74594b13f14e8cf30c40bdfce
Rahul Madaan <44020002+rahulmadaan@users.noreply.github.com>
2019-06-02 10:54:05 +0530

    * Move clientId and clientSecret to env variables * Add github callback method and getting the access_token

--- auth.js
+++ auth.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const CLIENT_ID = "4ea643e3197aea49a325"; // move this to env variables
-
+const request = require("superagent");
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const app = express();
 
 const logger = function(req, res, next) {
@@ -14,11 +15,28 @@ const serveClientId = function(req, res) {
   res.send(CLIENT_ID);
 };
 
+const processGithubRequest = function(req, res) {
+  const { code } = req.query;
+  request
+    .post("https://github.com/login/oauth/access_token")
+    .send({
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      code: code
+    })
+    .set("Accept", "application/json")
+    .then(function(response) {
+      console.log(response.body);
+    });
+};
+
 app.use(logger);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 
 app.get("/getClientId", serveClientId);
+app.get("/github/callback", processGithubRequest);
+
 app.use(express.static("."));
 
 module.exports = app;
